e59bd4b9938b69acca245c8eda450d9ba77abd01
Christoph Kormesser <christoph.kormesser@gmail.com>
2022-10-20 20:47:27 +0200

    merged feature/eurorupien into dev, added rupieneuro()

--- src/main/java/com/example/currencies/EuroRupienController.java
+++ src/main/java/com/example/currencies/EuroRupienController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class EuroRupienController {
     @GetMapping("/eurorupien")
     public String eurorupien(double euro) {
-        return String.format("%.2f€ in Rupien: %.2f$", euro, euro*81.01);
+        return String.format("%.2f€ in Rupien: %.2f", euro, euro*81.01);
+    }
+
+    @GetMapping("/rupieneuro")
+    public String rupieneuro(double rupien) {
+        return String.format("%.2fR in Euro: %.2f€", rupien, rupien*0.012);
     }
 }
